6c09b6138ef7a79d81cbe988834c90767ef5e2c7
pravinpimpale <pravinpimpale45@gmail.com>
2023-05-06 11:25:35 +0530

    Initial commit

---
+++ include/themes/dark/default/index.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2004-2023 The Canaris                                     |
+ |                                                                         |
+ +-------------------------------------------------------------------------+
+ | NMS: The Complete RRDtool-based Graphing Solution                     |
+ +-------------------------------------------------------------------------+
+ | This code is designed, written, and maintained by the Canaris. See      |
+ | about.php and/or the AUTHORS file for specific developer information.   |
+ +-------------------------------------------------------------------------+
+ | http://www.canaris.in/                                                   |
+ +-------------------------------------------------------------------------+
+*/
+
+header('Location:../index.php');
